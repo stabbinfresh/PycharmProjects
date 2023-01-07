7067d89c2ed30df991e69529f7934f5dbf8db893
Andrew Kuhs <andythekuhs@gmail.com>
2023-01-07 12:55:58 -0500

    day7 update

---
+++ day7ex2.py
@@ -0,0 +1,5 @@
+usernames = ["john 1990", "alberta1970", "magnola2000"]
+
+name_length = [len(username) for username in usernames]
+
+print(name_length)
---
+++ day7ex4.py
@@ -0,0 +1,5 @@
+user_entries = ['10', '19.1', '20']
+
+user_floats = [float(entry) for entry in user_entries]
+
+print(sum(user_floats))
