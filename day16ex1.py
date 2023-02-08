7af71af6bfc535a67043538eb63417d31a21d614
Andrew Kuhs <andythekuhs@gmail.com>
2023-02-07 21:25:08 -0500

    day 18 commit

--- day16ex1.py
+++ day16ex1.py
@@ -1,17 +1,34 @@
 import PySimpleGUI as sg
+from converters import convert
 
-label1 = sg.Text("Enter feet:")
-input1 = sg.Input()
+sg.theme("Black")
 
-label2 = sg.Text("Enter inches:")
-input2 = sg.Input()
+feet_label = sg.Text("Enter feet:")
+feet_input = sg.Input(key="feet")
+
+inches_label = sg.Text("Enter inches:")
+inches_input = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
+output_label = sg.Text("", key="output")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("File Compressor",
-                   layout=[[label1, input1],
-                           [label2, input2],
-                           [convert_button]])
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [convert_button, exit_button, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+
+    result = round(convert(feet, inches), 3)
+    window["output"].update(value=f"{result} m", text_color="white")
+
 window.close()
